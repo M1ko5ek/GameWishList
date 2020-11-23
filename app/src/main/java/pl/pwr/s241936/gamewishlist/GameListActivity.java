b53a9f6c5a8b0207979168d8cf28e8eb0da8bc95
M1ko5ek <mikolajlempart@gmail.com>
2020-11-23 16:29:42 +0100

    Login + password + GameListActivity

---
+++ app/src/main/java/pl/pwr/s241936/gamewishlist/GameListActivity.java
@@ -0,0 +1,14 @@
+package pl.pwr.s241936.gamewishlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_list);
+    }
+}
--- app/src/main/java/pl/pwr/s241936/gamewishlist/MainActivity.java
+++ app/src/main/java/pl/pwr/s241936/gamewishlist/MainActivity.java
@@ -2,13 +2,47 @@ package pl.pwr.s241936.gamewishlist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText email;
+    private EditText password;
+    private Button login;
+    private TextView info;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        email = (EditText)findViewById(R.id.editTextTextEmailAddress);
+        password = (EditText)findViewById(R.id.editTextTextPassword);
+        login = (Button)findViewById(R.id.buttonLogin);
+        info = (TextView)findViewById(R.id.textVievInfo);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validate(email.getText().toString(), password.getText().toString());
+            }
+        });
     }
+
+    private void validate (String userName, String userPassword){
+        if((userName.equals("admin")) && (userPassword.equals("1234"))){
+            info.setText("");
+            Intent intent = new Intent(this, GameListActivity.class);
+            startActivity(intent);
+
+        }else{
+            info.setText("Wrong email or password");
+        }
+    }
+
 }
