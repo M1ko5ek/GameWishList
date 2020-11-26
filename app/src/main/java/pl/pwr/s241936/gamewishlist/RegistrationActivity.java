6c120efb193f75ede2547feb997e69f2c708cd66
M1ko5ek <mikolajlempart@gmail.com>
2020-11-26 17:20:04 +0100

    Registration validate + firebase build.gradle

--- app/src/main/java/pl/pwr/s241936/gamewishlist/RegistrationActivity.java
+++ app/src/main/java/pl/pwr/s241936/gamewishlist/RegistrationActivity.java
@@ -8,34 +8,37 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegistrationActivity extends AppCompatActivity {
 
-    private EditText name;
-    private EditText email;
-    private EditText password1;
-    private EditText password2;
+    private EditText userName;
+    private EditText userEmail;
+    private EditText userPassword1;
+    private EditText userPassword2;
     private Button register;
     private TextView info;
     private TextView login;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        SetupUIViews();
 
-        name = (EditText)findViewById(R.id.editTextUserName);
-        email = (EditText)findViewById(R.id.editTextUserPassword);
-        password1 = (EditText)findViewById(R.id.editTextTextPassword);
-        password2 = (EditText)findViewById(R.id.editTextUserPasswordRepeat);
-        register = (Button)findViewById(R.id.buttonRegister);
-        info = (TextView)findViewById(R.id.textVievRegisterInfo);
-        login = (TextView)findViewById(R.id.textViewLogin);
+        firebaseAuth = FirebaseAuth.getInstance();
+
 
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                validate();
+                if(validate()){
+                    // send to firebase
+                    info.setText("Registered");
+                }
             }
         });
 
@@ -47,8 +50,33 @@ public class RegistrationActivity extends AppCompatActivity {
         });
     }
 
-    private void validate (){
-        info.setText("Registered");
+    private void SetupUIViews(){
+        userName = (EditText)findViewById(R.id.editTextUserName);
+        userEmail = (EditText)findViewById(R.id.editTextUserEmail);
+        userPassword1 = (EditText)findViewById(R.id.editTextUserPassword);
+        userPassword2 = (EditText)findViewById(R.id.editTextUserPasswordRepeat);
+        register = (Button)findViewById(R.id.buttonRegister);
+        info = (TextView)findViewById(R.id.textVievRegisterInfo);
+        login = (TextView)findViewById(R.id.textViewLogin);
+    }
+
+    private Boolean validate (){
+        Boolean result = false;
+        String name = RegistrationActivity.this.userName.getText().toString();
+        String email = RegistrationActivity.this.userEmail.getText().toString();
+        String password1 = RegistrationActivity.this.userPassword1.getText().toString();
+        String password2 = RegistrationActivity.this.userPassword2.getText().toString();
+        if(name.isEmpty() || email.isEmpty() || password1.isEmpty() || password2.isEmpty()){
+        {
+            Toast.makeText(this, "Please enter all the details", Toast.LENGTH_SHORT).show();
+        }
+        }else if (!password1.equals(password2)){
+            Toast.makeText(this, "Please repeat the password correctly", Toast.LENGTH_SHORT).show();
+            info.setText("haslo");
+        } else{
+            result = true;
+        }
+        return result;
     }
 
     private void openLoginActiviy(){
